d5ef63d9478e5fe8cc08c245a6f6c9602d133c9b
sagarhani <sagarhani33@gmail.com>
2021-04-12 16:53:05 +0530

    👌🏻 Listing learnings

---
+++ lib/learning.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+class Learning {
+  String id;
+  String title;
+  String description;
+  DateTime date;
+
+  Learning(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.date});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import "./learning.dart";
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final List<Learning> learnings = [
+    Learning(
+        id: '1',
+        title: "Flutter",
+        description: "Learning Flutter from scratch",
+        date: DateTime.now()),
+    Learning(
+        id: '2',
+        title: "Dart",
+        description: "Learning basics for Dart programming",
+        date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TIL Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.purple,
       ),
       home: MyHomePage(title: 'TIL Tracker'),
@@ -30,15 +35,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -46,29 +42,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Learning> learnings = [
+    Learning(
+        id: '1',
+        title: "Flutter",
+        description: "Learning Flutter from scratch",
+        date: DateTime.now()),
+    Learning(
+        id: '2',
+        title: "Dart",
+        description: "Learning basics for Dart programming",
+        date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Column(children: [
-          Container(
-            child: Card(
-              child: Text("TIL Tracker"),
-              elevation: 10,
-            ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(children: [
+        Container(
+          child: Card(
+            child: Text("TIL Tracker"),
+            elevation: 10,
           ),
-        ])
-        // This trailing comma makes  auto-formatting nicer for build methods.
-        );
+        ),
+        Column(
+            children: learnings.map((learning) {
+          return Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              width: double.infinity,
+              child: Card(
+                color: Colors.amber,
+                child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: Column(children: [
+                      Text(
+                        learning.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(learning.date),
+                        style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                            fontSize: 12,
+                            color: Colors.black),
+                      ),
+                    ])),
+                elevation: 5,
+              ));
+        }).toList())
+      ]),
+    );
   }
 }
